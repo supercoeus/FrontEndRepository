01f1d445d0cf8794c1c07b12e2b97565a7c1b914
sfl <sfl@enerbos.com>
2015-09-28 10:16:25 +0800

    1、增加higharts的使用，有错误！

---
+++ Learing/HighCharts/hunhetu/js/test.js
@@ -0,0 +1,7 @@
+$(function () {
+	$('#container').highcharts({ 
+		chart: { },
+		title: { text: 'Combination chart' },
+		xAxis: { categories: ['Apples', 'Oranges', 'Pears', 'Bananas', 'Plums'] }, 
+		
+			});
